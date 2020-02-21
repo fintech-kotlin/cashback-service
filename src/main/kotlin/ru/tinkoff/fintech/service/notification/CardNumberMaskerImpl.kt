b69839cc4ef48c431fd2d328327f24f055d28826
Roman Polovincev <polovincev@gmail.com>
2020-02-22 02:41:26 +0300

    implements method mask and generateMessage

--- src/main/kotlin/ru/tinkoff/fintech/service/notification/CardNumberMaskerImpl.kt
+++ src/main/kotlin/ru/tinkoff/fintech/service/notification/CardNumberMaskerImpl.kt
@@ -3,6 +3,12 @@ package ru.tinkoff.fintech.service.notification
 class CardNumberMaskerImpl: CardNumberMasker {
 
     override fun mask(cardNumber: String, maskChar: Char, start: Int, end: Int): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if (end < start) {
+            throw IndexOutOfBoundsException("Start index cannot be greater than end index")
+        }
+
+        return cardNumber
+            .mapIndexed { index, number -> if (index in start until end) maskChar else number }
+            .joinToString("")
     }
 }
