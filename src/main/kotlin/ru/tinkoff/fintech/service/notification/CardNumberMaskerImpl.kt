35ab15c26b04837161450faec181a7942c41c62f
Roman Polovincev <polovincev@gmail.com>
2020-02-22 14:14:38 +0300

    update throw exception

--- src/main/kotlin/ru/tinkoff/fintech/service/notification/CardNumberMaskerImpl.kt
+++ src/main/kotlin/ru/tinkoff/fintech/service/notification/CardNumberMaskerImpl.kt
@@ -3,9 +3,7 @@ package ru.tinkoff.fintech.service.notification
 class CardNumberMaskerImpl: CardNumberMasker {
 
     override fun mask(cardNumber: String, maskChar: Char, start: Int, end: Int): String {
-        if (end < start) {
-            throw IndexOutOfBoundsException("Start index cannot be greater than end index")
-        }
+        require(end >= start) { "Start index cannot be greater than end index" }
 
         return cardNumber
             .mapIndexed { index, number -> if (index in start until end) maskChar else number }
