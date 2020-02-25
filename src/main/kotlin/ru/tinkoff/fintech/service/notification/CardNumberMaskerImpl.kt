d531bf50c8b50110b7e313d1d362c1c863060408
Alexey Zavyalov <zavialov@bpcbt.com>
2020-02-25 23:34:01 +0300

    Implement CardNumberMaskerImpl and NotificationMessageGeneratorImpl

--- src/main/kotlin/ru/tinkoff/fintech/service/notification/CardNumberMaskerImpl.kt
+++ src/main/kotlin/ru/tinkoff/fintech/service/notification/CardNumberMaskerImpl.kt
@@ -1,8 +1,17 @@
 package ru.tinkoff.fintech.service.notification
 
-class CardNumberMaskerImpl: CardNumberMasker {
+class CardNumberMaskerImpl : CardNumberMasker {
 
     override fun mask(cardNumber: String, maskChar: Char, start: Int, end: Int): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if (cardNumber.isEmpty() || start == end) {
+            return cardNumber
+        }
+
+        if (start > end) {
+            throw Exception("Start index cannot be greater than end index")
+        }
+
+        val range = if (end > cardNumber.length) cardNumber.length else end
+        return cardNumber.replaceRange(start, range, maskChar.toString().repeat(range - start))
     }
 }
